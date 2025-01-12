11554dfe941ad8f41f7c5faff3f10d1b9697e57e
Vedant Pandya <32775169+pandyaved98@users.noreply.github.com>
2025-01-12 14:30:37 +0530

    Update advanced_multimodal_transformer.py

--- RAW/advanced_multimodal_transformer.py
+++ RAW/advanced_multimodal_transformer.py
@@ -16,4 +16,9 @@ class PaliGemmaConditionalGeneration(nn.Module):
         self.language_model = language_model
         self.pad_token_id = self.config.pad_token_id if self.config.pad_token_id is not None else -1
 
-        
+    def tie_weights(self):
+        return self.language_model.tie_weights()
+
+    def forward(self, input_ids: torch.LongTensor = None, pixel_values: torch.FloatTensor = None, attention_mask: Optional[torch.Tensor] = None, kv_cache: Optional[KVCache] = None) -> Tuple:
+        assert torch.all(attention_mask == 1), "The input cannot be padded."
+        input_embeds = self.language_model.get_input_embeddings()(input_ids)
