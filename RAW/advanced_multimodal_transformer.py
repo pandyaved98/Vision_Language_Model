903256bfc45408386f85bff061b1758cb5eda5ed
Vedant Pandya <32775169+pandyaved98@users.noreply.github.com>
2025-01-16 22:33:21 +0530

    Update advanced_multimodal_transformer.py

--- RAW/advanced_multimodal_transformer.py
+++ RAW/advanced_multimodal_transformer.py
@@ -5,6 +5,41 @@ from torch.nn import CrossEntropyLoss
 import math
 from advanced_vision_transformer import SiglipVisionConfig, SiglipVisionModel
 
+class GemmaConfig():
+    def __init__(self, vocab_size, hidden_size, intermediate_size, num_hidden_layers, num_attention_heads, num_key_value_heads, head_dim = 256, max_position_embeddings = 8192, rms_norm_eps - 1e - 6, rope_theta = 10000.0, attention_bias = False, attention_dropout = 0.0, pad_token_id = None, **Kwargs):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.max_position_embeddings = max_position_embeddings
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.head_dim = head_dim
+        self.num_key_value_heads = num_key_value_heads
+        self.rms_norm_eps = rms_norm_eps
+        self.rope_theta = rope_theta
+        self.attention_bias = attention_bias
+        self.attention_dropout = attention_dropout
+        self.pad_token_id = pad_token_id
+
+class PaliGemmaConfig():
+    def __init__(self, vision_config = None, text_config = None, ignore_index = -100, image_token_index = 256000, vocab_size = 257152, projection_dim = 2048, hidden_size = 2048, pad_token_id = None, **kwargs):
+        super().__init__()
+        self.ignore_index = ignore_index
+        self.image_token_index = image_token_index
+        self.vocab_size = vocab_size
+        self.projection_dim = projection_dim
+        self.hidden_size = hidden_size
+        self.vision_config = vision_config
+        self.is_encoder_decoder = False
+        self.pad_token_id = pad_token_id
+        self.vision_config = SiglipVisionConfig(**vision_config)
+        self.text_config = text_config
+        self.text_config = GemmaConfig(**text_config, pad_token_id = pad_token_id)
+        self.vocab_size = self.text_config.vocab_size
+        self.text_config.num_image_tokens = (self.vision_config.image_size // self.vision_config.patch_size) ** 2
+        self.vision_config.projection_dim = projection_dim 
+
 class PaliGemmaConditionalGeneration(nn.Module):
     def __init__(self, config: PaliGemmaConfig):
         super().__init__()
@@ -19,6 +54,23 @@ class PaliGemmaConditionalGeneration(nn.Module):
     def tie_weights(self):
         return self.language_model.tie_weights()
 
+    def _merge_input_ids_with_image_features(self, image_features = torch.Tensor, input_embeds: torch.Tensor, input_ids: torch.Tensor, attention_mask: torch.Tensor, kv_cache: Optional[KVCache] = None):
+        _, _, embed_dim = image_features.shape
+        batch_size, sequence_length = input_ids.shape
+        dtype, device = input_embeds.dtype, input_embeds.device
+        scaled_image_features = image_features / (self.config.hidden_size**0.5)
+        final_embedding = torch.zeros(batch_size, sequence_length, embed_dim, dtype = input_embeds.dtype, device = input_embeds.dtype, device = input_embeds.device)
+
     def forward(self, input_ids: torch.LongTensor = None, pixel_values: torch.FloatTensor = None, attention_mask: Optional[torch.Tensor] = None, kv_cache: Optional[KVCache] = None) -> Tuple:
         assert torch.all(attention_mask == 1), "The input cannot be padded."
         input_embeds = self.language_model.get_input_embeddings()(input_ids)
+        selected_image_feature = self.vision_tower(pixel_values.to(input_embeds.dtype))
+        image_features = self.multi_modal_projector(selected_image_feature)
+        input_embeds, attention_mask, position_ids = self._merge_input_ids_with)image_features(image_features, input_embeds, input_ids, attention_mask, kv_cache)
+        outputs = self.language_model(
+            attention_mask = attention_mask,
+            position_ids = position_ids,
+            input_embeds = input_embeds,
+            kv_cache = kv_cache,
+        )
+        return outputs
